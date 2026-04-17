5c1f6cddc550f13a4415e2ee6712c91b2c49560e
Random-Scientist <Random-Scientist@users.noreply.github.com>
2026-04-16 22:33:59 -0700

    mess with ponder i guess

---
+++ kubejs/client_scripts/crossgrove/test_ponder.ts
@@ -0,0 +1,11 @@
+
+
+Ponder.registry((event) => {
+    // event.create("crossroads:gear_base").scene("intro_to_rotary", "Introduction to Crossroads Rotary", (scene, util) => {
+    //     scene.showStructure();
+    //     scene.showBasePlate();
+    //     scene.configureBasePlate(4, 4, 10);
+    //     let world = scene.world;
+    //     world.setBlock([2, 1, 2], Block.id("crossroads:master_axis").with("facing", "up").getBlockState(), false);
+    // });
+});
