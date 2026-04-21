59c3d2d56e41c8469b9449c8a6817ba80257f978
Spookipup <270766034+Spookipup@users.noreply.github.com>
2026-04-20 18:09:38 -0600

    Noting crossgrove multiblock helpers

---
+++ kubejs/startup_scripts/crossgrove/gtceu_multiblock_helpers.js
@@ -0,0 +1,4 @@
+// priority: 9990
+
+global.CG_GT_MULTIBLOCK = Java.loadClass('com.crossgrove.integrations.gtceu.CrossgroveGtceuPredicates')
+global.CG_GT_PART_ABILITIES = Java.loadClass('com.crossgrove.integrations.gtceu.CrossgroveGtceuPartAbilities')
